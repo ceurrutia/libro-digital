c2c53a5e24609e81a89941738b4feca245a17d9c
Cecilia urrutia <urrutia.cici@gmail.com>
2025-01-31 14:02:06 -0300

    Inicio maquetado de prefooter

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.get('/', (req, res) => {
 
 // Inicia servidor
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at port ${PORT}`);
 });
